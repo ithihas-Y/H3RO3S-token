52d57e2efb94c4d7fe04b41168f7f002fd6921a0
ithihas-Y <57771329+ithihas-Y@users.noreply.github.com>
2021-12-14 13:38:56 +0530

    Update HerosToken.sol

--- contracts/HerosToken.sol
+++ contracts/HerosToken.sol
@@ -9,60 +9,11 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract H3RO3S is ERC20,Ownable {
 
-    using SafeERC20 for ERC20;
-
-    address payable public treasury;
-
-    uint256 public publicPriceWei;
-
-    mapping(uint256 => uint256) public specificPrices;  // Price including all decimals for different walletTypes
-
-    mapping(address => uint256) public specificWallets; // 0 for publicSale, choose number for others
-
-    mapping(IERC20 => bool) public supportedBuyTokens;  // supported Tokens used for buying
-
 
     constructor(string memory __name,string memory __symbol,uint256 supply,address payable _treasury) ERC20(__name,__symbol){
         _mint(_msgSender(),supply*(10**decimals()));
         treasury = _treasury;
     }
-
-    function setPublicPrice(uint256 _priceWei) onlyOwner() public {
-        publicPriceWei = _priceWei;
-    }
-
-    function setSpecificPrices(uint256[] memory walletTypes,uint256[] memory priceInWei) onlyOwner() public {
-        require(walletTypes.length == priceInWei.length,"Array length not equal");
-        for (uint256 i = 0; i < walletTypes.length; i++) {
-            specificPrices[walletTypes[i]] = priceInWei[i];
-        }
-    }
-
-    function setSpecificWallets(address[] memory wallets,uint256[] memory pricesInWei) onlyOwner() public {
-        require(wallets.length == pricesInWei.length,"Array length not equal");
-        for (uint256 i = 0; i < wallets.length; i++) {
-            specificWallets[wallets[i]] = pricesInWei[i];
-        }
-    }
-
-    function setSupportedBuyTokens(address[] memory tokens,bool[] memory supportedOr) public onlyOwner() {
-        require(tokens.length == supportedOr.length,"Array length not equal");
-        for (uint256 i = 0; i < tokens.length; i++) {
-            supportedBuyTokens[IERC20(tokens[i])] = supportedOr[i];
-        }
-    }
-
-    function buy(uint256 amountOfHero,address BuyToken) public {
-        require(supportedBuyTokens[IERC20(BuyToken)],"Unsupported Token for Buying");
-        uint256 cost;
-        if(specificWallets[_msgSender()] == 0){
-            cost = amountOfHero * publicPriceWei;
-        }else{
-            cost = amountOfHero * specificPrices[specificWallets[_msgSender()]];
-        }
-        require(ERC20(BuyToken).transferFrom(_msgSender(),treasury,cost),"Transfer of Buying TOken Failed");
-    }
-
 
 }
 
